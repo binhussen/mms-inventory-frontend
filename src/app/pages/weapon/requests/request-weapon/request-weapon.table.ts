6d30c6fb35b2f4fe6b1627167396e88006a52073
binhussen <mhdhsna@gmail.com>
2022-06-21 05:02:26 +0300

    fix merge approval

--- src/app/pages/weapon/requests/request-weapon/request-weapon.table.ts
+++ src/app/pages/weapon/requests/request-weapon/request-weapon.table.ts
@@ -9,11 +9,7 @@ const actions: Array<Action> = [
   { name: 'Expand', type: 'expand', path: 'request-for-weapon' },
   { name: 'Edit', type: 'edit' },
   {
- AuthenticationAndCustomer_crude
     name: 'Approve',type: 'approve',
-    urlToPopulateForm: `${baseApiUrl}/requestWeapons/[id]`,
-    name: 'Approve',
-    type: 'approve',
     form: requestWeaponForm.requestApprovalForm,
     submittedUrl: `${baseApiUrl}requestapprove/[id]`,
   }
